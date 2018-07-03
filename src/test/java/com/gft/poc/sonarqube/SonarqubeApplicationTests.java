f6a94223caa3ce897634be1f5d0c9f80efc77e05
Valerio Bigiani <valerio.bigiani@gft.com>
2018-07-03 13:17:58 +0200

    initial commit

---
+++ src/test/java/com/gft/poc/sonarqube/SonarqubeApplicationTests.java
@@ -0,0 +1,16 @@
+package com.gft.poc.sonarqube;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SonarqubeApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
