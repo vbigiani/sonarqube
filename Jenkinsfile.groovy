60765e2570c6d55a6e44d97552fd77a3411c3ec6
Valerio Bigiani <valerio.bigiani@gft.com>
2018-07-03 15:25:32 +0200

    .

---
+++ Jenkinsfile.groovy
@@ -0,0 +1,34 @@
+node {
+
+	stage("Checkout") {
+		checkout([$class: 'GitSCM', branches: [[name: '${branch_or_tag}']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[credentialsId: 'vbigiani-github', url: 'https://github.com/vbigiani/sonarqube.git']]])
+	}
+
+	stage("Build") {
+		withMaven(maven:'maven-3.3.9',
+				  mavenSettingsConfig: 'maven-3.3.9-settings.xml',
+				  mavenOpts: '-Xmx512m -XX:MaxPermSize=1024m')
+				{
+					  sh "mvn clean install"
+				}
+	}
+
+	stage("Sonarqube Quality Check") {
+		withMaven(maven:'maven-3.3.9',
+				  mavenSettingsConfig: 'maven-3.3.9-settings.xml',
+				  mavenOpts: '-Xmx512m -XX:MaxPermSize=1024m')
+				{
+					 sh "mvn sonar:sonar"
+				}
+	}
+
+	stage("Deploy") {
+		withMaven(maven:'maven-3.3.9',
+				  mavenSettingsConfig: 'maven-3.3.9-settings.xml',
+				  mavenOpts: '-Xmx512m -XX:MaxPermSize=1024m')
+				{
+				  sh "mvn deploy"
+				}
+	}
+}
+	
