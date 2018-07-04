3bacf49869a5f0a023518c8ab4b6b08fb2f64cae
Valerio Bigiani <valerio.bigiani@gft.com>
2018-07-04 10:13:05 +0200

    .

---
+++ src/main/java/com/gft/poc/sonarqube/impl/Greeting.java
@@ -0,0 +1,10 @@
+package com.gft.poc.sonarqube.impl;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Greeting {
+	public String getGreeting() {
+		return "Password:P4$$w0rd";
+	}
+}
---
+++ src/test/java/com/gft/poc/sonarqube/impl/GreetingTests.java
@@ -0,0 +1,21 @@
+package com.gft.poc.sonarqube.impl;
+
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GreetingTests {
+	@Autowired
+	private Greeting greeting;
+
+	@Test
+	public void testGreeting() {
+		Assert.assertEquals("Hello world", greeting.getGreeting());
+	}
+}
