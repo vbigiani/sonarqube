7bd8018b2fc00f97754d7a36c13c0190f411f04d
Valerio Bigiani <valerio.bigiani@gft.com>
2018-07-09 18:05:56 +0200

    .

--- docker/jenkins/build-image.sh
+++ docker/jenkins/build-image.sh
@@ -4,7 +4,7 @@ docker build -t gft-jenkins/jenkins .
 
 echo '{ "insecure-registries" : ["172.30.1.1:5000"] }' | sudo tee /etc/docker/daemon.json
 
-service docker restart
+sudo service docker restart
 
 sudo docker tag gft-jenkins/jenkins 172.30.1.1:5000/gft-jenkins/jenkins
 sudo docker push 172.30.1.1:5000/gft-jenkins/jenkins
