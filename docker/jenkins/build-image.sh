c521ac549e34e5ce481e5ca1163b87516c8b0f21
Valerio Bigiani <valerio.bigiani@gft.com>
2018-07-09 17:54:37 +0200

    .

--- docker/jenkins/build-image.sh
+++ docker/jenkins/build-image.sh
@@ -2,5 +2,5 @@
 
 docker build -t gft-jenkins/jenkins .
 
-sudo docker tag gft-jenkins/jenkins 172.30.1.1:5000/gft-jenkins
+sudo docker tag gft-jenkins/jenkins 172.30.1.1:5000/gft-jenkins/jenkins
 sudo docker push 172.30.1.1:5000/gft-jenkins/jenkins
